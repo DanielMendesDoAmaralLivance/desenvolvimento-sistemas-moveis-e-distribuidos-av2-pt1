8aac730797a8d0dbe8585965eb4f41454cab324e
DanielMendesDoAmaralLivance <daniel.amaral@livance.com.br>
2025-05-14 16:34:51 -0300

    Finaliza o serviço

---
+++ src/credit-card/credit-card.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreditCardService } from './credit-card.service';
+import { CreditCard, Prisma } from 'generated/prisma';
+
+@Controller('credit-card')
+export class CreditCardController {
+  constructor(private readonly creditCardService: CreditCardService) {}
+
+  @Post('/send')
+  async send(@Body() body: Prisma.CreditCardCreateInput): Promise<CreditCard> {
+    try {
+      return await this.creditCardService.create(body);
+    }
+    catch(e) {
+      console.error(e);
+    }
+  }
+}
---
+++ src/credit-card/credit-card.service.ts
@@ -0,0 +1,71 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { CreditCard, Prisma } from 'generated/prisma';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class CreditCardService {
+  constructor(
+    private readonly prisma: PrismaService,
+    @Inject('NOTIFICATION_SERVICE')
+    private readonly notificationClient: ClientProxy,
+  ) {}
+
+  async create(data: Prisma.CreditCardCreateInput): Promise<CreditCard> {
+    const creditCard = await this.registerPayment(data);
+
+    await this.processPayment(creditCard);
+
+    return creditCard;
+  }
+
+  private async registerPayment(
+    data: Prisma.CreditCardCreateInput,
+  ): Promise<CreditCard> {
+    const creditCard = await this.prisma.creditCard.create({ data });
+
+    this.sendCreatePaymentNotification(creditCard);
+
+    return creditCard;
+  }
+
+  private async processPayment(creditCard: CreditCard): Promise<void> {
+    setTimeout(async () => {
+      await this.prisma.creditCard.update({
+        where: {
+          id: creditCard.id,
+        },
+        data: {
+          paymentConfirmed: true,
+        },
+      });
+
+      this.sendConfirmationPaymentNotification(creditCard);
+    }, 5000);
+  }
+
+  private sendCreatePaymentNotification(creditCard: CreditCard): void {
+    this.sendNotification('payment_created', creditCard);
+  }
+
+  private sendConfirmationPaymentNotification(creditCard: CreditCard): void {
+    this.sendNotification('payment_confirmed', creditCard);
+  }
+
+  private sendNotification(
+    type: NotificationType,
+    creditCard: CreditCard,
+  ): void {
+    this.notificationClient
+      .emit(type, {
+        message: JSON.stringify(creditCard),
+      })
+      .subscribe({
+        next: () => console.log(`[${type}] notification sent.`),
+        error: (err) =>
+          console.error(`[${type}] failed to send notification:`, err),
+      });
+  }
+}
+
+type NotificationType = 'payment_created' | 'payment_confirmed';
